69c35eeccee8b22f639873bbca67b74d04c054c4
symon254 <79975499+symon254@users.noreply.github.com>
2022-03-24 18:39:21 +0300

    .

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-//import App from "./App";
-import AddPosts from "./component/AddPosts";
+import App from "./App";
+//import AddTest from "./component/AddTest";
+//import AddPosts from "./component/AddPosts";
 import { Provider } from "react-redux";
 import store from "./store";
 ReactDOM.render(
     <Provider store={store}>
-        <AddPosts />
+        <App />
     </Provider>,
     document.getElementById("root")
 );
