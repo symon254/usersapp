b115a5e4bab1ee2b86b4304e934aa554527b7dbf
symon254 <79975499+symon254@users.noreply.github.com>
2022-03-24 18:36:21 +0300

    navigation update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import React from "react";
-import List from "./component/list";
+import List from "./component/List";
+import AddPosts from "./component/AddPosts";
+import { Navbar, Nav, Container } from "react-bootstrap";
+
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import EditPosts from "./component/EditPosts";
 function App() {
     return (
         <div>
-            <List />
+            <Router>
+                <Navbar bg="primary" variant="dark">
+                    <Container>
+                        <Navbar.Brand href="/posts"> Post-JSON</Navbar.Brand>
+                        <Nav className="me-auto">
+                            <Link to={"/add"} className="nav-link">
+                                Add
+                            </Link>
+                            <Link to={"/posts"} className="nav-link">
+                                Posts
+                            </Link>
+                        </Nav>
+                    </Container>
+                </Navbar>
+                <div className="container mt-3">
+                    <Routes>
+                        <Route path={"/posts"} element={<List />} />
+                        <Route path="/add" element={<AddPosts />} />
+                        <Route path="/posts/:id" element={<EditPosts />} />
+                    </Routes>
+                </div>
+            </Router>
+            {/* <List /> */}
         </div>
     );
 }
