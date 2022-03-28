62faf5ee83bb096385ccacdbf4e8741c55e92848
symon254 <79975499+symon254@users.noreply.github.com>
2022-03-28 11:36:40 +0300

    update reducer for post

---
+++ src/Reducers/posts.js
@@ -0,0 +1,45 @@
+import {
+    GET_POST,
+    GET_POSTS,
+    GET_POSTS_FAIL,
+    CREATE_POST,
+    CREATE_POST_FAIL,
+    DELETE_POST,
+    UPDATE_POST,
+    UPDATE_POST_FAIL,
+} from "../Actions/types";
+
+const initialState = { posts: [] };
+
+export const postListReducer = (state = initialState, action) => {
+    const { type, payload } = action;
+    switch (type) {
+        case CREATE_POST:
+            return [...state, payload];
+        case CREATE_POST_FAIL:
+            return { error: payload };
+        case GET_POST:
+            return payload;
+        case GET_POSTS:
+            return { ...state, posts: payload };
+        case GET_POSTS_FAIL:
+            return { error: payload };
+        case DELETE_POST:
+            return state.filter(({ id }) => id !== payload.id);
+        case UPDATE_POST:
+            return state.map((post) => {
+                if (post.id === payload.id) {
+                    return {
+                        ...post,
+                        ...payload,
+                    };
+                } else {
+                    return post;
+                }
+            });
+        case UPDATE_POST_FAIL:
+            return { error: payload };
+        default:
+            return state;
+    }
+};
