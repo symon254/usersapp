a28ba17f2c7a4f35504893b15da123f33a0d4037
symon254 <79975499+symon254@users.noreply.github.com>
2022-03-28 11:35:01 +0300

    edit post component

--- src/component/EditPosts.js
+++ src/component/EditPosts.js
@@ -1,7 +1,93 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { updatePost, deletePost, getPost } from "../Actions/posts";
 
-function EditPosts() {
-    return <div>EditPosts</div>;
-}
+const EditPosts = (props) => {
+    const { id } = useParams();
 
+    const initialPostState = {
+        id: null,
+        title: "",
+    };
+    const [currentPost, setCurrentPost] = useState(initialPostState);
+    const [message, setMessage] = useState("");
+    const posts = useSelector((state) => state.posts);
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPost(id));
+        console.log(id);
+    }, [dispatch, id]);
+    useEffect(() => {
+        if (posts) {
+            setCurrentPost({ ...posts });
+        }
+    }, [posts]);
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setCurrentPost({ ...currentPost, [name]: value });
+    };
+
+    const updateContent = () => {
+        dispatch(updatePost(currentPost.id, currentPost))
+            .then((response) => {
+                console.log(response);
+                setMessage("The post was updated successfully!");
+            })
+            .catch((e) => {
+                console.log(e);
+            });
+    };
+    const removePost = () => {
+        dispatch(deletePost(currentPost.id))
+            .then(() => {
+                props.history.push("/posts");
+            })
+            .catch((e) => {
+                console.log(e);
+            });
+    };
+    return (
+        <div>
+            {currentPost ? (
+                <div className="edit-form">
+                    <h4>Post</h4>
+                    <form>
+                        <div className="form-group">
+                            <label htmlFor="title">Title</label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="title"
+                                name="title"
+                                value={currentPost.title || ""}
+                                onChange={handleInputChange}
+                            />
+                        </div>
+                    </form>
+
+                    <button
+                        className="badge badge-danger mr-2"
+                        onClick={removePost}
+                    >
+                        Delete
+                    </button>
+                    <button
+                        type="submit"
+                        className="badge badge-success"
+                        onClick={updateContent}
+                    >
+                        Update
+                    </button>
+                    <p>{message}</p>
+                </div>
+            ) : (
+                <div></div>
+            )}
+        </div>
+    );
+};
 export default EditPosts;
